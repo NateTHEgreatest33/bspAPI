cc234e91f0f85cbd9e2f33ca99dfe760318a447e
Nate Lenze <nlenze@umich.edu>
2020-11-29 16:57:16 -0600

    add initial framework

--- bspAPI.c
+++ bspAPI.c
@@ -15,10 +15,15 @@
 --------------------------------------------------------------------*/
 #include "bspAPI.h"
 
+#include "driverlib/gpio.h"
+#include "inc/hw_gpio.h"
+
 /*--------------------------------------------------------------------
                           LITERAL CONSTANTS
 --------------------------------------------------------------------*/
-
+#define L_BUTTON_MASK 0x10
+#define R_BUTTON_MASK 0x00
+#define LED_MASK 0x0E
 /*--------------------------------------------------------------------
                                 TYPES
 --------------------------------------------------------------------*/
@@ -30,7 +35,7 @@
 /*--------------------------------------------------------------------
                               VARIABLES
 --------------------------------------------------------------------*/
-
+static bool initilized = false;   /* has init function been run yet */
 /*--------------------------------------------------------------------
                                 MACROS
 --------------------------------------------------------------------*/
@@ -38,23 +43,96 @@
 /*--------------------------------------------------------------------
                               PROCEDURES
 --------------------------------------------------------------------*/
-void example
-    (
-    void
-    );
 
 /*********************************************************************
 *
 *   PROCEDURE NAME:
-*       example
+*       bspInit()
 *
 *   DESCRIPTION:
-*       x
+*       Initilaize LED and push buttons
 *
 *********************************************************************/
-void example
+void bsp_Init
     (
     void
     )
 {
-} /* example() */
+//init
+initilized = true;
+} /* bsp_Init() */
+
+/*********************************************************************
+*
+*   PROCEDURE NAME:
+*       bsp_LED()
+*
+*   DESCRIPTION:
+*       toggle LED
+*
+*********************************************************************/
+void bsp_LED
+    (
+    LED_colors color
+    )
+{
+/*----------------------------------------------------------
+Verify bspInit() has been run
+----------------------------------------------------------*/
+if( initilized == true )
+    {
+    /*--------------------------------------------------
+    shift color over by one to fit in LED_MASK portion
+    of register
+    --------------------------------------------------*/
+    GPIO_PORTF_BASE |= ( ( uint8_t ) color << 1 );
+    }
+} /* bsp_LED() */
+
+/*********************************************************************
+*
+*   PROCEDURE NAME:
+*       bsp_button_state()
+*
+*   DESCRIPTION:
+*       get current state of buttons
+*
+*********************************************************************/
+button_state bsp_button_state
+    (
+    button button; /* button selected */
+    )
+{
+/*----------------------------------------------------------
+Verify bspInit() has been run
+----------------------------------------------------------*/
+if( initilized == true )
+    {
+    switch ( button )
+        {
+        case L_BUTTON:
+            if ( ( GPIO_PORTF_BASE & L_BUTTON_MASK ) == L_BUTTON_MASK )
+                {
+                return button_ENABLED;
+                }
+            else
+                {
+                return button_DISABLED;
+                }
+            break;
+        case R_BUTTON:
+            if ( ( GPIO_PORTF_BASE & R_BUTTON_MASK ) == R_BUTTON_MASK )
+                {
+                return button_ENABLED;
+                }
+            else
+                {
+                return button_DISABLED;
+                }
+            break;
+        default:
+            /* error case */
+            break;
+        }
+    }
+} /* bsp_button_state() */
--- bspAPI.h
+++ bspAPI.h
@@ -10,6 +10,8 @@
 /*--------------------------------------------------------------------
                            GENERAL INCLUDES
 --------------------------------------------------------------------*/
+#include <stdint.h>
+#include <stdbool.h>
 
 /*--------------------------------------------------------------------
                           LITERAL CONSTANTS
@@ -18,6 +20,32 @@
 /*--------------------------------------------------------------------
                                 TYPES
 --------------------------------------------------------------------*/
+typedef uint8_t LED_colors;        /* LED color list */
+enum
+    {
+    OFF,                                     /* 0x00 */
+    RED,                                     /* 0x01 */
+    BLUE,                                    /* 0x02 */
+    PINK,                                    /* 0x03 */
+    GREEN,                                   /* 0x04 */
+    YELLOW,                                  /* 0x05 */
+    TEAL,                                    /* 0x06 */
+    WHITE                                    /* 0x07 */
+    };
+
+typedef uint8_t button_state;   /* state of buttons  */
+enum
+    {
+    button_ENABLED,             /* button pushed     */
+    button_DISABLED             /* button not pushed */
+    };
+
+typedef uint8_t button;         /* state of buttons */
+enum
+    {
+    L_BUTTON,                   /* left button      */
+    R_BUTTON                    /* right button     */
+    };
 
 /*--------------------------------------------------------------------
                            MEMORY CONSTANTS
@@ -37,5 +65,18 @@
 /*--------------------------------------------------------------------
 bspAPI.c
 --------------------------------------------------------------------*/
+void bsp_Init
+    (
+    void
+    );
 
+void bsp_LED
+    (
+    LED_colors color /* color selected */
+    );
+
+button_state bsp_button_state
+    (
+    button button; /* button selected */
+    );
 /* bspAPI.h */
